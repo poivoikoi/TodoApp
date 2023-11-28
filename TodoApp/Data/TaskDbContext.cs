ea202000117cff467e09431cc90fe8fca2f2f876
poivoikoi <142922658+poivoikoi@users.noreply.github.com>
2023-11-28 07:23:14 +0200

    Добавьте файлы проекта.

---
+++ TodoApp/Data/TaskDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Task = TodoApp.Models.Task;
+
+
+namespace TodoApp.Data
+{
+    public class TaskDbContext : DbContext
+    {
+        public TaskDbContext(DbContextOptions<TaskDbContext> options) : base(options) { }
+
+        public DbSet<Task> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Task>().HasKey(t => t.Id);
+        }
+    }
+}
---
+++ TodoApp/Models/Task.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApp.Models
+{
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
